3db858b2324127261b4d9795129825e1532c6155
Ben Darnell <ben@cockroachlabs.com>
2019-09-13 10:16:11 -0400

    Replace formula with the current version from homebrew-core

--- Formula/cockroach.rb
+++ Formula/cockroach.rb
@@ -1,42 +1,51 @@
 class Cockroach < Formula
   desc "Distributed SQL database"
   homepage "https://www.cockroachlabs.com"
-  version "1.0"
-  revision 1
-  url "https://binaries.cockroachdb.com/cockroach-v1.0.src.tgz"
-  sha256 "ca87b10eec688195e0df4f85431b019f2980ae4b511ee321f91f945315efeb76"
+  url "https://binaries.cockroachdb.com/cockroach-v19.1.4.src.tgz"
+  version "19.1.4"
+  sha256 "d819167dc109b566511cb6cef9aadc8c4f07b1be6a8b3108f4bbd14808d21faf"
   head "https://github.com/cockroachdb/cockroach.git"
 
-  def install
-    raise <<-EOS.undent
-      The CockroachDB formula has moved into Homebrew proper.
-      Please instead run:
-
-          $ brew uninstall cockroachdb/cockroach/cockroach
-          $ brew untap cockroachdb/cockroach
-          $ brew install cockroach
-
-    EOS
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "82d20462ca26fcdaad2c4639582bc3cbb10fee2ca9fb63d1944648dc9fdf461a" => :mojave
+    sha256 "247cea9630fdc06c497c42ccf6290b601870eaac96a14ec91a993e87bbf5acb1" => :high_sierra
+    sha256 "f55c8f203ff3c49312641f81d7a15c4f3bcc02c5a5585981574b11161eeda62d" => :sierra
   end
 
-  def caveats; <<-EOS.undent
-    CockroachDB is a distributed database intended for multi-server deployments.
+  depends_on "autoconf" => :build
+  depends_on "cmake" => :build
+  depends_on "go" => :build
+  depends_on "make" => :build
+  depends_on "xz" => :build
+
+  def install
+    # The GNU Make that ships with macOS Mojave (v3.81 at the time of writing) has a bug
+    # that causes it to loop infinitely when trying to build cockroach. Use
+    # the more up-to-date make that Homebrew provides.
+    ENV.prepend_path "PATH", Formula["make"].opt_libexec/"gnubin"
+    # Build only the OSS components
+    system "make", "buildoss"
+    system "make", "install", "prefix=#{prefix}", "BUILDTYPE=release"
+  end
+
+  def caveats; <<~EOS
     For local development only, this formula ships a launchd configuration to
     start a single-node cluster that stores its data under:
       #{var}/cockroach/
     Instead of the default port of 8080, the node serves its admin UI at:
-      #{Formatter.url('http://localhost:26256')}
+      #{Formatter.url("http://localhost:26256")}
 
     Do NOT use this cluster to store data you care about; it runs in insecure
     mode and may expose data publicly in e.g. a DNS rebinding attack. To run
     CockroachDB securely, please see:
-      #{Formatter.url('https://www.cockroachlabs.com/docs/secure-a-cluster.html')}
-    EOS
+      #{Formatter.url("https://www.cockroachlabs.com/docs/secure-a-cluster.html")}
+  EOS
   end
 
   plist_options :manual => "cockroach start --insecure"
 
-  def plist; <<-EOS.undent
+  def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
@@ -60,24 +69,34 @@ class Cockroach < Formula
       <true/>
     </dict>
     </plist>
-    EOS
+  EOS
   end
 
   test do
     begin
-      system "#{bin}/cockroach", "start", "--insecure", "--background"
-      pipe_output("#{bin}/cockroach sql --insecure", <<-EOS.undent)
+      # Redirect stdout and stderr to a file, or else  `brew test --verbose`
+      # will hang forever as it waits for stdout and stderr to close.
+      system "#{bin}/cockroach start --insecure --background &> start.out"
+      pipe_output("#{bin}/cockroach sql --insecure", <<~EOS)
         CREATE DATABASE bank;
         CREATE TABLE bank.accounts (id INT PRIMARY KEY, balance DECIMAL);
         INSERT INTO bank.accounts VALUES (1, 1000.50);
       EOS
       output = pipe_output("#{bin}/cockroach sql --insecure --format=csv",
         "SELECT * FROM bank.accounts;")
-      assert_equal <<-EOS.undent, output
-        1 row
+      assert_equal <<~EOS, output
         id,balance
         1,1000.50
       EOS
+    rescue => e
+      # If an error occurs, attempt to print out any messages from the
+      # server.
+      begin
+        $stderr.puts "server messages:", File.read("start.out")
+      rescue
+        $stderr.puts "unable to load messages from start.out"
+      end
+      raise e
     ensure
       system "#{bin}/cockroach", "quit", "--insecure"
     end
