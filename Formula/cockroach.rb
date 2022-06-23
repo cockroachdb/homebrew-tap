278bd74021a2552a4ca00db899813257dae31d11
Rail Aliiev <rail@iqchoice.com>
2022-06-23 08:21:32 -0400

    release: update CockroachDB version to v22.1.3
    Release note: None

--- Formula/cockroach.rb
+++ Formula/cockroach.rb
@@ -4,9 +4,9 @@
 class Cockroach < Formula
   desc "Distributed SQL database"
   homepage "https://www.cockroachlabs.com"
-  url "https://binaries.cockroachdb.com/cockroach-v22.1.2.darwin-10.9-amd64.tgz"
-  version "22.1.2"
-  sha256 "c81837588042e04bcd39de4fbf888179da1914ceb7560f4fca4624bbb894980c"
+  url "https://binaries.cockroachdb.com/cockroach-v22.1.3.darwin-10.9-amd64.tgz"
+  version "22.1.3"
+  sha256 "e0d98cd4070b03d1ed03b30ebdf38c084117bdb13c6c7e78a14f61d213fcdace"
 
   def install
     bin.install "cockroach"
