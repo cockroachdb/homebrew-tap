3c11cf8661e5efa8b8654339f3d56fb8cc88f45b
Cameron Nunez <cameron@cockroachlabs.com>
2022-12-08 12:10:53 -0500

    ccloud: bump ccloud to 0.3.6

--- Formula/ccloud.rb
+++ Formula/ccloud.rb
@@ -4,9 +4,9 @@
 class Ccloud < Formula
   desc "CockroachDB Cloud CLI"
   homepage "https://www.cockroachlabs.com"
-  url "https://binaries.cockroachdb.com/ccloud/ccloud_darwin-amd64_0.2.5.tar.gz"
-  version "0.2.5"
-  sha256 "e2a64f7a8f76812d9a08cd029452262e70717f448adb75e0f50733f8ff25e544"
+  version "0.3.6"
+  url "https://binaries.cockroachdb.com/ccloud/ccloud_darwin-amd64_0.3.6.tar.gz"
+  sha256 "726f370941e14bab9478a29923f33529ad357b49f9431d813477472259554cff"
 
   def install
     bin.install "ccloud"
@@ -14,7 +14,7 @@ class Ccloud < Formula
 
   test do
     output = shell_output("#{bin}/ccloud version", 0)
-    assert_match "ccloud 0.2.5", output
+    assert_match "ccloud 0.3.6", output
   end
 end
 
