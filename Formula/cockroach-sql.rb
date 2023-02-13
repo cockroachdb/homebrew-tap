3264f96dd2ed13f4826217bfa93cc7d5cce4e1b6
Jane Xing <zhouxing@uchicago.edu>
2023-02-13 14:03:18 -0500

    Version bump for v22.2.4

--- Formula/cockroach-sql.rb
+++ Formula/cockroach-sql.rb
@@ -4,15 +4,15 @@
 class CockroachSql < Formula
   desc "Distributed SQL database shell"
   homepage "https://www.cockroachlabs.com"
-  version "22.2.3"
+  version "22.2.4"
   on_macos do
     on_intel do
-      url "https://binaries.cockroachdb.com/cockroach-sql-v22.2.3.darwin-10.9-amd64.tgz"
-      sha256 "3d18eea41824aa5a6b70eb9043f29d84f54bf14a7317cc9f5a71086a20964bea"
+      url "https://binaries.cockroachdb.com/cockroach-sql-v22.2.4.darwin-10.9-amd64.tgz"
+      sha256 "b79533d62a812a5e47152f984cb027254a5e47144a0fc5d7b99c44833e31dd73"
     end
     on_arm do
-      url "https://binaries.cockroachdb.com/cockroach-sql-v22.2.3.darwin-11.0-arm64.tgz"
-      sha256 "28ac8ad22a70f0d52ccaff189cac670103c3e8a7e01bb39f010d7398d3512c94"
+      url "https://binaries.cockroachdb.com/cockroach-sql-v22.2.4.darwin-11.0-arm64.tgz"
+      sha256 "2476e54242d5177bfabe46a3dddaa66a963e94fa0643385583c9a00c51963968"
     end
   end
 
@@ -22,7 +22,7 @@ class CockroachSql < Formula
 
   test do
     output = shell_output("#{bin}/cockroach-sql --version", 0)
-    assert_match "22.2.3", output
+    assert_match "22.2.4", output
   end
 
 end
