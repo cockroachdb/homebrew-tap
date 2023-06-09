1be9baae43b46aab34bc15a3858cdba23791c1e5
Nate Long <long@cockroachlabs.com>
2023-06-09 11:03:42 -0700

    Bump ccloud to v0.4.8

--- Formula/ccloud.rb
+++ Formula/ccloud.rb
@@ -4,16 +4,16 @@
 class Ccloud < Formula
   desc "CockroachDB Cloud CLI"
   homepage "https://www.cockroachlabs.com"
-  version "0.3.7"
+  version "0.4.8"
 
   on_macos do
     on_intel do
-      url "https://binaries.cockroachdb.com/ccloud/ccloud_darwin-amd64_0.3.7.tar.gz"
-      sha256 "a0999ec88afb762da7686797d0ac910c38ba13851c1e10946277e28aa0eb3051"
+      url "https://binaries.cockroachdb.com/ccloud/ccloud_darwin-amd64_0.4.8.tar.gz"
+      sha256 "e3b1a7177a2a253aa51aa5f6e12ead5d5818a9c6824f7fa18387168df31ed60b"
     end
     on_arm do
-      url "https://binaries.cockroachdb.com/ccloud/ccloud_darwin-arm64_0.3.7.tar.gz"
-      sha256 "0722bdc9713174c2bc03702e9ab6f7da6216ea8a2421c9015075da8288781ff3"
+      url "https://binaries.cockroachdb.com/ccloud/ccloud_darwin-arm64_0.4.8.tar.gz"
+      sha256 "1ac9fdc934efc37de2c94807ec39c5eb83d0d22658f4dadecf90fe425f6e1a66"
     end
   end
 
@@ -23,7 +23,7 @@ class Ccloud < Formula
 
   test do
     output = shell_output("#{bin}/ccloud version", 0)
-    assert_match "ccloud 0.3.7", output
+    assert_match "ccloud 0.4.8", output
   end
 end
 
