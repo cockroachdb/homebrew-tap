24c1a0ba4514738e5198d97aa57b18679f0b8414
Justin Beaver <teamcity@cockroachlabs.com>
2024-06-17 19:47:02 +0000

    release: advance to v23.2.6

--- Formula/cockroach@23.2.rb
+++ Formula/cockroach@23.2.rb
@@ -4,20 +4,21 @@
 class CockroachAT232 < Formula
   desc "Distributed SQL database"
   homepage "https://www.cockroachlabs.com"
-  version "23.2.5"
+  version "23.2.6"
   on_macos do
     on_intel do
-      url "https://binaries.cockroachdb.com/cockroach-v23.2.5.darwin-10.9-amd64.tgz"
-      sha256 "df4126568c3bded296d61e4fba5ee14f0ac3029dff25ba82d55c5580d04f5408"
+      url "https://binaries.cockroachdb.com/cockroach-v23.2.6.darwin-10.9-amd64.tgz"
+      sha256 "ddcd54d9a42fbc03fca280a7008d546ddf5eef2101cda72e5afc7163cfc5c3f6"
     end
     on_arm do
-      url "https://binaries.cockroachdb.com/cockroach-v23.2.5.darwin-11.0-arm64.tgz"
-      sha256 "1f213ef13a565bf0e4f5cab0b957fcb34584c4561a46f1af014bd4dc2d8449c1"
+      url "https://binaries.cockroachdb.com/cockroach-v23.2.6.darwin-11.0-arm64.tgz"
+      sha256 "f0d4b485f24b26b0908e803482a834b49098d3e577f8231835d09207b26ff0ad"
     end
   end
 
   def install
     bin.install "cockroach"
+    prefix.install "LICENSE.txt", "THIRD-PARTY-NOTICES.txt"
     on_intel do
       lib.mkpath
       mkdir "#{lib}/cockroach"
